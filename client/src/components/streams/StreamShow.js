ef88b46778b6b88d98fda1bf502baad8851bda4a
Eddie Keller <116ekg@gmail.com>
2018-12-20 11:13:20 -0700

    Added ability to stream video

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.videoRef = React.createRef()
+  }
 
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
+    this.buildPlayer()
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer()
+  }
+
+  componentWillUnmount() {
+    console.log('destroying video feed...')
+    this.player.destroy()
+  }
+
+  renderVideo = () => {
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return
+    }
+    this.renderVideo()
   }
 
   render() {
@@ -17,6 +49,11 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video 
+          ref={this.videoRef} 
+          style={{ width: '100%' }} 
+          controls 
+        />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
